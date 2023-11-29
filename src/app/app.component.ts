75013fe851b1333f6112412323418110abba1680
JeffErasLindao <ggpachec@espol.edu.ec>
2023-11-29 10:16:29 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
+import { TopbarComponent } from './shared/topbar/topbar.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { IndexComponent } from './pages/index/index.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent, IndexComponent,TopbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
