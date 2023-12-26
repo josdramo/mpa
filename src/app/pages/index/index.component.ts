dcb7baa59cd28f13c4b2be35d23af88d88e1437c
“josdramo” <josdramo@espol.edu.ec>
2023-12-26 15:16:19 -0500

    cambios en index

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLinkActive, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLinkActive, RouterLink],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
